7591756a1798fced7791c392875f1ab83978e72d
iagoronanvs <iago.vsantos@gmail.com>
2020-12-02 05:12:28 -0300

    listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
@@ -11,12 +11,16 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -64,6 +68,62 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://xesque.rocketseat.dev/users/avatar/profile-731a9502-130e-4dae-a31f-293e384a3232.jpg"
+                  alt="GoBarber"
+                />
+
+                <strong>Iago Ronan</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://xesque.rocketseat.dev/users/avatar/profile-731a9502-130e-4dae-a31f-293e384a3232.jpg"
+                  alt="GoBarber"
+                />
+
+                <strong>Iago Ronan</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://xesque.rocketseat.dev/users/avatar/profile-731a9502-130e-4dae-a31f-293e384a3232.jpg"
+                  alt="GoBarber"
+                />
+
+                <strong>Iago Ronan</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
