106398228475110dca3a9901da1e016e1002e6e6
Iago Ronan <iago.santos@privatecode.com.br>
2021-08-01 11:34:57 -0300

    finalizando testes do hook

--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,11 +1,18 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react-hooks';
 import MockAdapter from 'axios-mock-adapter';
 import { AuthProvider, useAuth } from '../../hooks/auth';
 import api from '../../services/api';
 
 const apiMock = new MockAdapter(api);
+const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+const removeItemSpy = jest.spyOn(Storage.prototype, 'removeItem');
 
 describe('Auht hook', () => {
+  beforeEach(() => {
+    setItemSpy.mockClear();
+    removeItemSpy.mockClear();
+  });
+
   it('should be able to sign in', async () => {
     apiMock.onPost('sessions').reply(200, {
       access_token: 'token-123',
@@ -14,8 +21,6 @@ describe('Auht hook', () => {
         email: 'jhondoe@example.com.br',
       },
     });
-
-    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
 
     const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
@@ -30,5 +35,80 @@ describe('Auht hook', () => {
 
     expect(setItemSpy).toHaveBeenCalledTimes(2);
     expect(result.current.user.email).toEqual('jhondoe@example.com.br');
+  });
+
+  it('should restore saved data from storage when auth inits', () => {
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation(key => {
+      switch (key) {
+        case '@GoBarber:token':
+          return 'token-123';
+
+        case '@GoBarber:user':
+          return JSON.stringify({
+            name: 'Jhon Doe',
+            email: 'jhondoe@example.com',
+          });
+        default:
+          return null;
+      }
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    expect(result.current.user.email).toEqual('jhondoe@example.com');
+  });
+
+  it('should be able sign out', async () => {
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation(key => {
+      switch (key) {
+        case '@GoBarber:token':
+          return 'token-123';
+
+        case '@GoBarber:user':
+          return JSON.stringify({
+            name: 'Jhon Doe',
+            email: 'jhondoe@example.com',
+          });
+        default:
+          return null;
+      }
+    });
+
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    act(() => {
+      result.current.signOut();
+    });
+
+    expect(removeItemSpy).toHaveBeenCalledTimes(2);
+    expect(result.current.user).toBeUndefined();
+  });
+
+  it('should be able to update user data', () => {
+    const { result } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    const user = {
+      id: 'user-123',
+      name: 'Jonh Doe',
+      email: 'jonhdoe@example.com.br',
+      avatar_url: 'test.jpg',
+    };
+
+    act(() => {
+      result.current.updateUser(user);
+    });
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:user',
+      JSON.stringify(user),
+    );
+
+    expect(result.current.user).toEqual(user);
   });
 });
